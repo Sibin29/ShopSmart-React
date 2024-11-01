e7f8d95b01dc8462c2d63b3a8508849055c2dc23
Sibin29 <Sibin292000>
2024-11-01 07:56:10 -0400

    added signup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,7 @@ export default function Layout() {
     <Stack>
       <Stack.Screen name="index" options={{ title: 'Welcome' }} />
       <Stack.Screen name="LoginScreen" options={{ title: 'Login' }} />
+      <Stack.Screen name="SignUp" options={{ title: 'SignUp' }} />
       <Stack.Screen name="HomeScreen" options={{ title: 'Shops' }} />
       <Stack.Screen name="UserProfileScreen" options={{ title: 'Profile' }} />
     </Stack>
